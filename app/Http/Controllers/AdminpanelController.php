057b64b39017e9bfc718d58dfa4871c4b237617c
iTzMeRafa <rafael97@gmx.de>
2019-07-16 01:49:51 +0200

    added admin auth middleware

--- app/Http/Controllers/AdminpanelController.php
+++ app/Http/Controllers/AdminpanelController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminpanelController extends GlobalController
 {
+    public function __construct()
+    {
+        $this->middleware('adminAuth');
+    }
+
     public function index() {
         return view('pages/admin/home')->with('data', $this->getViewData());
     }
