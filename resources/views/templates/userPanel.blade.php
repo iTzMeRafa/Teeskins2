e7ec039390f577fee307533354ca1accf837a183
iTzMeRafa <rafael97@gmx.de>
2019-07-18 21:52:34 +0200

    fixed mobile view for userPanel

--- resources/views/templates/userPanel.blade.php
+++ resources/views/templates/userPanel.blade.php
@@ -1,13 +1,10 @@
-<nav class="navbar navbar-expand-md userPanel">
+<nav class="navbar navbar-expand userPanel">
     <div class="container">
         @guest
             <a href="/login" class="btn btn-secondary">Upload</a>
         @else
             <a href="/upload" class="btn btn-primary">Upload</a>
         @endguest
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
