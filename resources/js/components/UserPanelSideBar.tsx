c0147a80e262d3349736fb291162c26d56258ef2
iTzMeRafa <rafael97@gmx.de>
2019-07-19 00:48:32 +0200

    added active class to user sidebar navigatin

--- resources/js/components/UserPanelSideBar.tsx
+++ resources/js/components/UserPanelSideBar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 // Services
-import { UrlService } from './../Services/UrlService';
+import { URLS, UrlService } from './../Services/UrlService';
 
 export default class UserPanelSideBar extends React.Component {
 
@@ -21,13 +21,13 @@ export default class UserPanelSideBar extends React.Component {
                 <h5 className={`${this.blockName}__title`}> Userpanel </h5>
                 <div>
                     <ul className={`navbar ${this.blockName}__navbar`}>
-                        <li className="nav-item">
-                            <a className="nav-link" href={this.urlService.mergeBaseWithPathURL('userpanel/dashboard')}>
+                    <li className={`nav-item ${this.urlService.navIsActive(URLS.Dashboard)}`}>
+                            <a className="nav-link" href={this.urlService.mergeBaseWithPathURL(URLS.Dashboard)}>
                                 Dashboard
                             </a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href={this.urlService.mergeBaseWithPathURL('userpanel/settings')}>
+                        <li className={`nav-item ${this.urlService.navIsActive(URLS.Settings)}`}>
+                            <a className="nav-link" href={this.urlService.mergeBaseWithPathURL(URLS.Settings)}>
                                 Settings
                             </a>
                         </li>
