9824ebd307b45b0c4c0a816138bf466432146db4
iTzMeRafa <rafael97@gmx.de>
2019-10-09 00:33:29 +0200

    refactored unlike action

--- app/Http/Controllers/UnlikeController.php
+++ app/Http/Controllers/UnlikeController.php
@@ -15,63 +15,7 @@ class UnlikeController extends Controller
     public function index($assetType, $assetID) {
         if (DB::table('likes')->where(['assetType' => $assetType, 'assetID' => $assetID, 'userID' => Auth::id()])->delete()) {
 
-            switch ($assetType) {
-
-                case "skin":
-                    DB::table('skins')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "body":
-                    DB::table('body')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "decoration":
-                    DB::table('decoration')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "eyes":
-                    DB::table('eyes')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "feet":
-                    DB::table('feet')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "hands":
-                    DB::table('hands')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "marking":
-                    DB::table('marking')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "mapres":
-                    DB::table('mapres')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "gameskins":
-                    DB::table('gameskins')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "emoticons":
-                    DB::table('emoticons')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "cursors":
-                    DB::table('cursors')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "particles":
-                    DB::table('particles')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-                case "grids":
-                    DB::table('grids')->where('id' , '=', $assetID)->decrement('likes');
-                    break;
-
-
-            }
-
+            DB::table($assetType)->where('id' , '=', $assetID)->decrement('likes');
         }
     }
 }
