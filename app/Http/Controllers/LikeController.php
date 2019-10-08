150eecaf161c10e52b76329a66cb22de4b4f713f
iTzMeRafa <rafael97@gmx.de>
2019-10-09 00:21:52 +0200

    refactored like action

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -15,61 +15,7 @@ class LikeController extends Controller
     public function index($assetType, $assetID) {
         if (DB::table('likes')->insert(['assetType' => $assetType, 'assetID' => $assetID, 'userID' => Auth::id(), 'date' => NOW()])) {
 
-            switch ($assetType) {
-
-                case "skin":
-                    DB::table('skins')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "body":
-                    DB::table('body')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "decoration":
-                    DB::table('decoration')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "eyes":
-                    DB::table('eyes')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "feet":
-                    DB::table('feet')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "hands":
-                    DB::table('hands')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "marking":
-                    DB::table('marking')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "mapres":
-                    DB::table('mapres')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "gameskins":
-                    DB::table('gameskins')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "emoticons":
-                    DB::table('emoticons')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "cursors":
-                    DB::table('cursors')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "particles":
-                    DB::table('particles')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-
-                case "grids":
-                    DB::table('grids')->where('id' , '=', $assetID)->increment('likes');
-                    break;
-            }
-
+            DB::table($assetType)->where('id' , '=', $assetID)->increment('likes');
         }
     }
 }
