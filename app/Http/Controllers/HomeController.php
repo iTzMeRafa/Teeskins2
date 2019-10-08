8df19d3303dd8a787d3e3dd79a2d652e31bc28d3
iTzMeRafa <rafael97@gmx.de>
2019-10-09 00:21:03 +0200

    refactored home page controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,118 +27,51 @@ class HomeController extends GlobalController
         return view('pages/home')->with("data", $this->getViewData());
     }
 
+    private function SortObjectByKeysDesc($a, $b) {
+        return strcmp($b->uploadDate, $a->uploadDate);
+    }
+
+    private function SortObjectByKeysAsc($a, $b) {
+        return strcmp($a->uploadDate, $b->uploadDate);
+    }
+
     private function getNewestAsset() {
-        $trendingArray = [
-            "skin"        => $this->getTrendingAsset('skins', 'uploadDate'),
-            "body"        => $this->getTrendingAsset('body', 'uploadDate'),
-            "decoration"  => $this->getTrendingAsset('decoration', 'uploadDate'),
-            "eyes"        => $this->getTrendingAsset('eyes', 'uploadDate'),
-            "feet"        => $this->getTrendingAsset('feet', 'uploadDate'),
-            "hands"       => $this->getTrendingAsset('hands', 'uploadDate'),
-            "marking"     => $this->getTrendingAsset('marking', 'uploadDate'),
-            "mapres"      => $this->getTrendingAsset('mapres', 'uploadDate'),
-            "gameskins"   => $this->getTrendingAsset('gameskins', 'uploadDate'),
-            "emoticons"   => $this->getTrendingAsset('emoticons', 'uploadDate'),
-            "cursors"     => $this->getTrendingAsset('cursors', 'uploadDate'),
-            "particles"   => $this->getTrendingAsset('particles', 'uploadDate'),
-            "grids"       => $this->getTrendingAsset('grids', 'uploadDate'),
-        ];
+        $newestAssets = [];
 
-        $uploadDateArray = [
-            "skin"        => $trendingArray["skin"]->uploadDate,
-            "body"        => $trendingArray["body"]->uploadDate,
-            "decoration"  => $trendingArray["decoration"]->uploadDate,
-            "eyes"        => $trendingArray["eyes"]->uploadDate,
-            "feet"        => $trendingArray["feet"]->uploadDate,
-            "hands"       => $trendingArray["hands"]->uploadDate,
-            "marking"     => $trendingArray["marking"]->uploadDate,
-            "mapres"      => $trendingArray["mapres"]->uploadDate,
-            "gameskins"   => $trendingArray["gameskins"]->uploadDate,
-            "emoticons"   => $trendingArray["emoticons"]->uploadDate,
-            "cursors"     => $trendingArray["cursors"]->uploadDate,
-            "particles"   => $trendingArray["particles"]->uploadDate,
-            "grids"       => $trendingArray["grids"]->uploadDate,
-        ];
+        foreach ($this->assetTypes as $assetType) {
+            $asset = $this->getTrendingAsset($assetType, 'uploadDate');
+            $asset->assetType = $assetType;
+            array_push($newestAssets, $asset);
+        }
 
-        $newestAsset = array_keys($uploadDateArray, max($uploadDateArray));
-        $trendingArray[$newestAsset[0]]->assetType = $newestAsset[0];
-        return $trendingArray[$newestAsset[0]];
+        usort($newestAssets, [$this, "SortObjectByKeysDesc"]);
+        return $newestAssets[0];
     }
 
     private function getMostDownloadedAsset() {
-        $trendingArray = [
-            "skin"        => $this->getTrendingAsset('skins', 'downloads'),
-            "body"        => $this->getTrendingAsset('body', 'downloads'),
-            "decoration"  => $this->getTrendingAsset('decoration', 'downloads'),
-            "eyes"        => $this->getTrendingAsset('eyes', 'downloads'),
-            "feet"        => $this->getTrendingAsset('feet', 'downloads'),
-            "hands"       => $this->getTrendingAsset('hands', 'downloads'),
-            "marking"     => $this->getTrendingAsset('marking', 'downloads'),
-            "mapres"      => $this->getTrendingAsset('mapres', 'downloads'),
-            "gameskins"   => $this->getTrendingAsset('gameskins', 'downloads'),
-            "emoticons"   => $this->getTrendingAsset('emoticons', 'downloads'),
-            "cursors"     => $this->getTrendingAsset('cursors', 'downloads'),
-            "particles"   => $this->getTrendingAsset('particles', 'downloads'),
-            "grids"       => $this->getTrendingAsset('grids', 'downloads'),
-        ];
+        $mostDownloadedAsset = [];
 
-        $downloadsArray = [
-            "skin"        => $trendingArray["skin"]->downloads,
-            "body"        => $trendingArray["body"]->downloads,
-            "decoration"  => $trendingArray["decoration"]->downloads,
-            "eyes"        => $trendingArray["eyes"]->downloads,
-            "feet"        => $trendingArray["feet"]->downloads,
-            "hands"       => $trendingArray["hands"]->downloads,
-            "marking"     => $trendingArray["marking"]->downloads,
-            "mapres"      => $trendingArray["mapres"]->downloads,
-            "gameskins"   => $trendingArray["gameskins"]->downloads,
-            "emoticons"   => $trendingArray["emoticons"]->downloads,
-            "cursors"     => $trendingArray["cursors"]->downloads,
-            "particles"   => $trendingArray["particles"]->downloads,
-            "grids"       => $trendingArray["grids"]->downloads,
-        ];
+        foreach ($this->assetTypes as $assetType) {
+            $asset = $this->getTrendingAsset($assetType, 'downloads');
+            $asset->assetType = $assetType;
+            array_push($mostDownloadedAsset, $asset);
+        }
 
-        $newestAsset = array_keys($downloadsArray, max($downloadsArray));
-        $trendingArray[$newestAsset[0]]->assetType = $newestAsset[0];
-        return $trendingArray[$newestAsset[0]];
+        usort($mostDownloadedAsset, [$this, "SortObjectByKeysAsc"]);
+        return $mostDownloadedAsset[0];
     }
 
     private function getMostLikedAsset() {
-        $trendingArray = [
-            "skin"        => $this->getTrendingAsset('skins', 'likes'),
-            "body"        => $this->getTrendingAsset('body', 'likes'),
-            "decoration"  => $this->getTrendingAsset('decoration', 'likes'),
-            "eyes"        => $this->getTrendingAsset('eyes', 'likes'),
-            "feet"        => $this->getTrendingAsset('feet', 'likes'),
-            "hands"       => $this->getTrendingAsset('hands', 'likes'),
-            "marking"     => $this->getTrendingAsset('marking', 'likes'),
-            "mapres"      => $this->getTrendingAsset('mapres', 'likes'),
-            "gameskins"   => $this->getTrendingAsset('gameskins', 'likes'),
-            "emoticons"   => $this->getTrendingAsset('emoticons', 'likes'),
-            "cursors"     => $this->getTrendingAsset('cursors', 'likes'),
-            "particles"   => $this->getTrendingAsset('particles', 'likes'),
-            "grids"       => $this->getTrendingAsset('grids', 'likes'),
-        ];
+        $mostLikedAssets = [];
 
-        $likesArray = [
-            "skin"        => $trendingArray["skin"]->likes,
-            "body"        => $trendingArray["body"]->likes,
-            "decoration"  => $trendingArray["decoration"]->likes,
-            "eyes"        => $trendingArray["eyes"]->likes,
-            "feet"        => $trendingArray["feet"]->likes,
-            "hands"       => $trendingArray["hands"]->likes,
-            "marking"     => $trendingArray["marking"]->likes,
-            "mapres"      => $trendingArray["mapres"]->likes,
-            "gameskins"   => $trendingArray["gameskins"]->likes,
-            "emoticons"   => $trendingArray["emoticons"]->likes,
-            "cursors"     => $trendingArray["cursors"]->likes,
-            "particles"   => $trendingArray["particles"]->likes,
-            "grids"       => $trendingArray["grids"]->likes,
-        ];
+        foreach ($this->assetTypes as $assetType) {
+            $asset = $this->getTrendingAsset($assetType, 'likes');
+            $asset->assetType = $assetType;
+            array_push($mostLikedAssets, $asset);
+        }
 
-        $newestAsset = array_keys($likesArray, max($likesArray));
-        $trendingArray[$newestAsset[0]]->assetType = $newestAsset[0];
-        return $trendingArray[$newestAsset[0]];
+        usort($mostLikedAssets, [$this, "SortObjectByKeysAsc"]);
+        return $mostLikedAssets[0];
     }
 
     private function getViewData() {
