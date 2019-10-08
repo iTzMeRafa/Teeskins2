26bad8d3c4b4b73ca3a3562cc5254b725e5c821a
iTzMeRafa <rafael97@gmx.de>
2019-10-09 00:20:22 +0200

    refactored downloads action

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -9,61 +9,7 @@ class DownloadsController extends Controller
     public function index($assetType, $assetID) {
         if (DB::table('downloads')->insert(['assetType' => $assetType, 'assetID' => $assetID, 'date' => NOW()])) {
 
-            switch ($assetType) {
-
-                case "skin":
-                    DB::table('skins')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "body":
-                    DB::table('body')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "decoration":
-                    DB::table('decoration')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "eyes":
-                    DB::table('eyes')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "feet":
-                    DB::table('feet')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "hands":
-                    DB::table('hands')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "marking":
-                    DB::table('marking')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "mapres":
-                    DB::table('mapres')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "gameskins":
-                    DB::table('gameskins')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "emoticons":
-                    DB::table('emoticons')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "cursors":
-                    DB::table('cursors')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "particles":
-                    DB::table('particles')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-                case "grids":
-                    DB::table('grids')->where('id' , '=', $assetID)->increment('downloads');
-                    break;
-
-            }
+            DB::table($assetType)->where('id' , '=', $assetID)->increment('downloads');
         }
     }
 }
